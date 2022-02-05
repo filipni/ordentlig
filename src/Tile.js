034351b1bbbff1c9c94192cdade8607ab2ff7448
Filip Nilsson <filmarnil@gmail.com>
2022-02-05 12:04:55 +0100

    Turn tile intofunctional component

--- src/Tile.js
+++ src/Tile.js
@@ -1,14 +1,12 @@
-import react from "react";
 import './Tile.css';
 
-class Tile extends react.Component {
-    render() {
-        const style = {backgroundColor:this.props.color};
-        return (
-            <div className="Tile" style={style}>
-                {this.props.hidden ? '' : this.props.letter.toUpperCase()}
-            </div>);
-    }
+function Tile(props) {
+    const style = {backgroundColor: props.color};
+    return (
+        <div className="Tile" style={style}>
+            {props.hidden ? '' : props.letter.toUpperCase()}
+        </div>
+    );
 }
 
 export default Tile;
